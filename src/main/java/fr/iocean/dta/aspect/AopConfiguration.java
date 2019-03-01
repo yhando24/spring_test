61491b45522a45b1fee1ae61ae954292b7d2c053
yhando24 <youcef.handoura@gmail.com>
2019-03-01 17:17:25 +0100

    fin semaine

--- src/main/java/fr/iocean/dta/aspect/AopConfiguration.java
+++ src/main/java/fr/iocean/dta/aspect/AopConfiguration.java
@@ -5,25 +5,40 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import ch.qos.logback.classic.Level;
 import fr.iocean.dta.exception.EmployeeNotFoundException;
+import fr.iocean.dta.main.App;
 
 @Aspect
 @Component
 public class AopConfiguration {
 	
-////	tp 20
-//	@Before("execution(* fr.iocean.dta.service.EmployeService.*(..))")
-//	public void afficheNomMethode(org.aspectj.lang.JoinPoint jp) {
-//		System.out.println("Nom de la methode : " + jp.getSignature().getName());
-//	}
-//	
-////	tp 21
-//	@AfterThrowing(pointcut = "execution(* *(..) throws fr.iocean.dta.exception.EmployeeNotFoundException)", throwing = "ex")
-//	 public void logAfterThrowing(EmployeeNotFoundException ex) throws Throwable  {
-//		System.out.println(ex.getMessage());
-//	}
+	private static final ch.qos.logback.classic.Logger loggerParent 
+	  = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("AopConfiguration");
+
+//	tp 20
+	@Before("execution(* fr.iocean.dta.service.EmployeService.*(..))")
+	public void afficheNomMethode(org.aspectj.lang.JoinPoint jp) {
+		loggerParent.info("JE SUIS UNE INFO : logger parent");
+		loggerParent.setLevel(Level.WARN);
+		loggerParent.warn("Nom de la methode : " + jp.getSignature().getName(), App.class.getSimpleName() );
+
+	}
+	
+//	tp 21
+	@AfterThrowing(pointcut = "execution(* *(..) throws fr.iocean.dta.exception.EmployeeNotFoundException)", throwing = "ex")
+	 public void logAfterThrowing(EmployeeNotFoundException ex) throws Throwable  {
+		
+		final ch.qos.logback.classic.Logger loggerenfant   = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("AopConfiguration.child");
+		loggerenfant.info("JE SUIS UNE INFO :  logger enfant");
+		loggerenfant.setLevel(Level.ERROR);
+		loggerenfant.error(ex.getMessage());
+
+	}
 	
 	@Around("execution(* fr.iocean.dta.service.*.*(..))")
 	public void afficherMessageBeforeAllFunction(ProceedingJoinPoint joinPoint) {
--- src/main/java/fr/iocean/dta/main/App.java
+++ src/main/java/fr/iocean/dta/main/App.java
@@ -1,8 +1,14 @@
 package fr.iocean.dta.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.*;
 
 import org.springframework.context.support.AbstractApplicationContext;
+
+import ch.qos.logback.classic.Level;
+
+
 
 /**
  * Hello world!
@@ -13,9 +19,15 @@ import org.springframework.context.support.AbstractApplicationContext;
 @EnableAspectJAutoProxy
 public class App 
 {
+	private static final Logger logger  = LoggerFactory.getLogger(App.class);
+
+	
+	
+	
     public static void main( String[] args )
     {
     	AbstractApplicationContext context = new AnnotationConfigApplicationContext(App.class);
-    	System.out.println(" Hello world!");
+    
+    	  logger.info("Example log from {}", App.class.getSimpleName());
     }
 }
